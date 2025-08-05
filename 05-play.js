72fbc94484a2f067f55d75f31ef4f6c28eb7e36c
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-05 04:34:16 +0300

    Lesson 16: Working with Objects, Properties & Methods

---
+++ 05-play.js
@@ -0,0 +1,35 @@
+const person = {
+    name: 'Max',
+    age: 29,
+    greet: () => {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const personTwo = {
+    name: 'Max',
+    age: 29,
+    greet: function() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const personThree = {
+    name: 'Immanuel',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+console.log(person);
+
+// to use the greet, you see you cant call it the way it is above
+person.greet(); 
+// why is this like that? because ToDo research arrow functions the this keyword refers to the global scope
+
+personTwo.greet();
+
+// or,
+
+personThree.greet();
