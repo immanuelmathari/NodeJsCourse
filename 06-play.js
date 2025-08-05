ae62da7b0184a198ab1af42558fc8f1c75bc3c0c
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-05 04:50:12 +0300

    Lesson 18: Arrays, Objects & Reference Types

---
+++ 06-play.js
@@ -0,0 +1,17 @@
+const hobbies = ['Sports', 'Cooking'];
+
+for (let hobby of hobbies) {
+    console.log(hobby);
+}
+
+// map will take a function that defines how to edit an array
+// that function will be executed in each element of the array one after another
+// and it returns the updated version of the element
+
+console.log(hobbies.map(hobby => {
+    return 'Hobby: ' + hobby;
+}));
+
+// or
+
+console.log(hobbies.map(hobby => 'My Hobby is : ' + hobby));
