84f28fa2c1341b807ab3a8b5bb1bf1c098bf4db4
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-08 15:04:15 +0300

    Lesson 20: Destructuring

---
+++ 09-play.js
@@ -0,0 +1,36 @@
+// 08.08.25
+
+const person = {
+    name : 'manu',
+    age: 24,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const printName = (personData) => {
+    console.log(personData.name);
+}
+
+printName(person);
+
+// but as you can see we only want the name. say this is coming from a third party library that we have no access over. 
+// we destructure ({ })
+// we can just specify the name of the property we want 
+const printName2 = ({ name, greet }) => {
+    console.log(name);
+} 
+printName2(person);
+// basically you pull the properties we actually need from a datapool
+
+
+// OR
+// you store in the const the names you expect to be equal to that object.
+const { name, age } = person; // will store the value of the object property of name and age in object person. this convention is not common but when it comes to destructuring it is.
+console.log(name,age);
+
+// to destructure arrays
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+// NB: when you destructure objects you use {} when destructuring arrays you use []. and once destructured, they wount be arrays
