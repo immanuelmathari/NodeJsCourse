fd391639032e68c27c47e7fb10a7534951579235
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-09 06:46:17 +0300

    Lesson 37: Blocking and non-blocking code

--- 17-app.js
+++ 17-app.js
@@ -16,24 +16,26 @@ const server = http.createServer((req, res) => {
         req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);
-        }); 
-        // you were wrong here
-        // req.on('end', () => {
+        });
         return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString(); 
+            const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+            fs.writeFile('message.txt', message, (err) => {
+                // this should be done only when we writing the file. we dont want to wait and do this later
+                // this is  
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+
         });
     }
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-        res.write('</html>');
-        res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);
