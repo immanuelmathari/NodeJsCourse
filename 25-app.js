eaf79ef752dde33dab41b2c42fd5e189c2bdcc96
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-18 21:43:08 +0300

    Lesson 68: Adding a 404 Error page

--- 25-app.js
+++ 25-app.js
@@ -1,15 +1,26 @@
-const app = express();
+const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
 
 // const adminRoutes = require('./routes/25-admin.js');
 const adminRoutes = require('./routes/25-admin');
 const shopRoutes = require('./routes/25-shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes); // dont call it like a function
 // the above also cant be below the below route. you know why
 
 app.use(shopRoutes);
+
+// a catch all middleware
+app.use((req, res, next) => {
+    // will handle all http methods post, get
+    // res.send('<h1>Page not found</h1>');
+    // to get status code
+    // 404 common status when page not found
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
