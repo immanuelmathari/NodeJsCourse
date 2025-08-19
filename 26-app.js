096969ab9df0378c2994475c04568f8b94f2adcb
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-19 09:25:40 +0300

    Lesson 69: Filtering paths

---
+++ 26-app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/26-admin');
+const shopRoutes = require('./routes/25-shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// this is a filter
+app.use('/admin',adminRoutes); 
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
+app.listen(3000);
